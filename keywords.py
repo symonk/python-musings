8aea9cc48a1fe4853756e79239c408aa82d17be6
symonk <jackofspaces@gmail.com>
2019-01-14 21:08:10 +0000

    logical operators

--- keywords.py
+++ keywords.py
@@ -35,10 +35,26 @@ x == y # True
 
 -------------------------------------------------------------------------------------------------------------
 
-# 'and', 'or', 'not' are the logical operators in python and will result in true if both operands are true
-# see truth tables for each below:
+# 'and', 'or', 'not' are the logical operators in python
+# 'and' truth table: ('and' results in True only if both operators are True)
 
-a = True, b = True, => a and b # True
-a = False, b = True => a and b # False
-a = True, b = False => a and b # False
-a = False, b = False => a and b # False (*careful here)
+a = True, b = True, : a and b # True
+a = False, b = True : a and b # False
+a = True, b = False : a and b # False
+a = False, b = False : a and b # False (*careful here)
+
+# 'or' truth table: ('or' results in True if any of the operators are True)
+a = True, b = False : a or b # True
+a = False, b = True : a or b # True
+a = False, b = False : a or b # False
+a = True, b  = True : a or b # True
+
+# 'not' truth table: ('not' flips the result, similar to '!' in java
+a = True : a not True # False
+a = False : a not True # True
+
+1 == 1 and 5 == 5 # True
+1 == 2 or 2 == 2 # True
+1 == 3 or 2 == 5 # False
+not False # True
+
