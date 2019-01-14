0362b9aab5f5bf20ad12cd3a0d539daff51de38d
symonk <jackofspaces@gmail.com>
2019-01-14 20:58:29 +0000

    seperators

--- keywords.py
+++ keywords.py
@@ -6,16 +6,22 @@ from math import cos
 print(keyword.kwlist)
 print(cos(25))
 
+-------------------------------------------------------------------------------------------------------------
+
 # The 'as' keyword can be used to give an import a customised name for reference within our module
 from math import cos as alias
 
 alias(25)
+
+-------------------------------------------------------------------------------------------------------------
 
 # 'True' and 'False' are typical Truth values in python, like pretty much anything
 
 1 == 1 #True
 5 == 4 #False
 True and False #False
+
+-------------------------------------------------------------------------------------------------------------
 
 # 'None' is the absence of a value or a 'null' value from other languages
 #  not returning anything from a method, automatically returns None in python
@@ -26,6 +32,8 @@ None == False # False
 x = None
 y = None
 x == y # True
+
+-------------------------------------------------------------------------------------------------------------
 
 
 
