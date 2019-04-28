71f236961d0b66210ed7807a774ceed00610994a
symonk <jackofspaces@gmail.com>
2019-04-28 11:51:38 +0100

    started writing up python dictionarys

--- properties.py
+++ properties.py
@@ -1,1 +1,9 @@
+#  Find here some interesting facts and learning material on the python dictionary
 
+# Python dictionaries are collection(s) that are backed by a hash table (with build in collision algorithms) if you are
+# familiar with the likes of java, they are your hash map -> a hash table backed collection of key value pairs
+
+-------------------------------------------------------------------------------------------------------------
+# Instantiation:
+dict(iterable) # used when you want to make a dict out of an iterable
+dict_b = {'a': 'A', 'b': 'B'}
