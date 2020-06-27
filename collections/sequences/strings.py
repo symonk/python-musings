5d1c578d99da86e01263fcee5e70ed78cc7b6be1
symonk <jackofspaces@gmail.com>
2020-06-27 16:52:05 +0100

    adding strings.py

---
+++ collections/sequences/strings.py
@@ -0,0 +1,7 @@
+# Strings in python are immutable sequences of unicode code characters.
+
+# Return a copy (immutable strings) of the string in capitilized form (initial letter capitilized) and the
+# rest lower cased.  This has been changed in python 3.8 to return the title() equivalent on the first character.
+
+'capitlized'.capitalize()
+
