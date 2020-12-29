a00b06b9c9cdc132721f1ff86ba3cc1296a366af
Simon K <jackofspaces@gmail.com>
2020-12-29 13:34:04 +0000

    Update __missing__.py

--- object_data_model/__missing__.py
+++ object_data_model/__missing__.py
@@ -1,8 +1,12 @@
 from collections import defaultdict
 """
-Invoked by subclasses of dict when dunder __getitem__ when they key is not present in the dictionary.
-This method is only applicable to dictionary subclasses (as outlined below) as base dict class invokes it and
-subsequently raises a KeyError if the default_factory in subclasses of the collections.defaultdict.
+__missing__ is only applicable for subclasses of dictionaries, noticiby the defaultdict (collections.defaultdict) which
+accepts a default_factory= callable to its constructor (__new__ -> __init__).  When a default_factory callable is not
+supplied, upon key misses as part of __getitem__ then subsequently __missing__ a KeyError is raised.  However, passing a
+callable (such as a lambda) to `default_factory=lambda: 1337` then such function is called rather than raising a KeyError
+in the instance outlined above.  __missing__ is only applicable to dictionaries specifically and trying such mechanism
+on a list subclass (which also supports key indexing) will not work, as __missing__ will never be invoked.  This is all
+outlined below.
 """
 
 
